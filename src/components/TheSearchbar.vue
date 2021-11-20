1bff4118180a33c26224a8f83c29dde22b6c6a59
Anna Chiara <anna.laves@web.de>
2021-11-20 16:51:20 +0100

    [master] added placeholder for searchbar

---
+++ src/components/TheSearchbar.vue
@@ -0,0 +1,11 @@
+<template>
+	<!-- ADD CUSTOM SEARCHBAR -->
+	<div>
+		<v-text-field
+			append-icon="mdi-magnify"
+			label="Search"
+			single-line
+			hide-details
+		></v-text-field>
+	</div>
+</template>
