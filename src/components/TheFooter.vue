077056a17a7e921bddf2858f2274c3746c15ee4a
Anna Chiara <anna.laves@web.de>
2021-11-20 15:14:47 +0100

    amending

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    <h3>Footer</h3>
+  </footer>
+</template>
+
+<style>
+footer {
+  background-color: darkgray;
+  margin: 0;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+h5 {
+  margin: 0;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div >
+    <section id="home">
+      <TheHeader />
+      <TheFooter />
+    </section>
+
+  <div class="sheet-wrapper">
+<v-sheet
+  color="white"
+  elevation="1"
+  height="600px"
+  width="500px"
+></v-sheet>
+  </div>
+  </div>
+</template>
+
+<script>
+import TheHeader from "../components/TheHeader.vue";
+import TheFooter from "../components/TheFooter.vue";
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+};
+</script>
+
+<style >
+  .sheet-wrapper {
+    display: flex;
+    justify-content: center;
+  }
+</style>
