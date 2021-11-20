cfad861a4073cc56936efaee718d0b42aeacd358
Anna Chiara <anna.laves@web.de>
2021-11-20 15:19:18 +0100

    [master] indentation adjustment

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,19 +1,19 @@
 <template>
-  <header>
-    <h5>Todo's</h5>
-  </header>
+	<header>
+		<h5>Todo's</h5>
+	</header>
 </template>
 
 <style>
 header {
-  background-color: darkgray;
-  margin: 0;
-  height: 50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+	background-color: darkgray;
+	margin: 0;
+	height: 50px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 }
 h5 {
-  margin: 0;
+	margin: 0;
 }
 </style>
