79b53fd001e75ba16cbff2bf6e48ee735705ddcd
Anna Chiara <anna.laves@web.de>
2021-11-24 21:44:37 +0100

    [master] finished priority feature

---
+++ src/components/Priority.vue
@@ -0,0 +1,58 @@
+<template>
+	<div>
+		<div class="priority-wrapper">
+			<v-icon
+				:color="getPriorityStatus(priorityStatus).color"
+				class="circle-icon mdi-18px"
+			>
+				mdi-circle
+			</v-icon>
+			<v-list-item-subtitle>
+				{{ getPriorityStatus(priorityStatus).status }}
+			</v-list-item-subtitle>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		priorityStatus: Number,
+	},
+	methods: {
+		getPriorityStatus(statusNumber) {
+			let status;
+			let color;
+
+			switch (statusNumber) {
+				case 1:
+					status = 'Low';
+					color = '#C0FFE0';
+					break;
+				case 2:
+					status = 'Medium';
+					color = '#FF8400';
+					break;
+				case 3:
+					status = 'High';
+					color = '#E00000';
+					break;
+				default:
+					status = '';
+					color = '';
+			}
+			return {
+				status,
+				color,
+			};
+		},
+	},
+};
+</script>
+
+<style scoped>
+.priority-wrapper {
+	display: flex;
+	align-items: center;
+}
+</style>
